6134071015bde854a34fc9d245f31c563b6e96b3
unknown <cariton_vilasanti@hotmail.com>
2025-05-08 19:34:22 -0300

    projeto criado

---
+++ src/test/java/com/exampleprojetoJWT/projetoJWT/ProjetoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleprojetoJWT.projetoJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
